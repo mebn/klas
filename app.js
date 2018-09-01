ed4c98d5c632d467f0aefdacdb9d65f5888b1f97
marcusnilszen <39590503+marcusnilszen@users.noreply.github.com>
2018-09-01 18:59:19 +0200

    cleanup

--- app.js
+++ app.js
@@ -1,59 +1,51 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
-
+const io = require('socket.io')(server);
 const PORT = process.env.PORT || 8080;
 app.use(express.static('src'));
 
-const io = require('socket.io')(server);
+// pigpio / GPIO
+
+// socket.io
 io.on('connection', socket => {
-    // webApp
-    socket.on('moveWeb', move => {
-        if(move.forward) direction('forward');
-        else if(move.backward) direction('backward');
-        else stop('y');
-
-        if(move.left) direction('left');
-        else if(move.right) direction('right');
-        else stop('x');
-    });
-    // klasApp (react native)
-    /*
-    socket.on('moveRN', move => {
-        if(move.forward) direction('forward');
-        else if(move.backward) direction('backward');
-        else stop('y');
-
-        if(move.left) direction('left');
-        else if(move.right) direction('right');
-        else stop('x');
-    });
-    */
+    socket.on('moveWeb', move => handleMove(move));
+    socket.on('moveRN', move => handleMove(move));
 });
+
+const handleMove = move => {
+    if (move.forward) direction('forward');
+    else if (move.backward) direction('backward');
+    else stop('y');
+
+    if (move.left) direction('left');
+    else if (move.right) direction('right');
+    else stop('x');
+};
 
 // forward, backward, left, right
 const direction = direction => {
-    if(direction === 'forward'){
+    if (direction === 'forward') {
         console.log('forward');
     }
-    else if(direction === 'backward'){
+    else if (direction === 'backward') {
         console.log('backward');
     }
-    else if(direction === 'left'){
+    else if (direction === 'left') {
         console.log('left');
     }
-    else if(direction === 'right'){
+    else if (direction === 'right') {
         console.log('right');
     }
 };
 
 // reset the motors when no button is pressed
 const stop = direction => {
-    if(direction === 'y'){
-        console.log('y');
+    if (direction === 'y') {
+        //console.log('y');
     }
-    else if(direction === 'x'){
-        console.log('x');
+    else if (direction === 'x') {
+        //console.log('x');
     }
 };
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 $(function () {
-    var socket = io();
-    var move = { forward: false, right: false, downward: false, left: false };
-
-    const handleTouchEnd = e => {
-        e.preventDefault();
-        move[e.target.id] = false;
-        e.target.style.background = '#00818a';
-    };
+    const socket = io();
+    const move = { forward: false, right: false, downward: false, left: false };
 
     const handleTouchStart = e => {
         e.preventDefault();
@@ -14,9 +8,15 @@ $(function () {
         e.target.style.background = '#404b69';
     };
 
-    $('div').on('touchstart', e => handleTouchStart(e));
-    $('div').on('touchend', e => handleTouchEnd(e));
-    $('div').on('touchcancel', e => handleTouchEnd(e));
+    const handleTouchEnd = e => {
+        e.preventDefault();
+        move[e.target.id] = false;
+        e.target.style.background = '#00818a';
+    };
 
-    //setInterval(() => socket.emit('moveWeb', move), 1000 / 60);
+    $('div').on('touchstart', handleTouchStart);
+    $('div').on('touchend', handleTouchEnd);
+    $('div').on('touchcancel', handleTouchEnd);
+
+    setInterval(() => socket.emit('moveWeb', move), 1000 / 60);
 });
