02ebff785314c1f3df120cca2ad5f0aef06bdf7f
marcusnilszen <39590503+marcusnilszen@users.noreply.github.com>
2018-09-01 17:56:49 +0200

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-$(function(){
+$(function () {
     var socket = io();
-    var move = { forward:false, right:false, downward:false, left:false };
+    var move = { forward: false, right: false, downward: false, left: false };
 
     const handleTouchEnd = e => {
         e.preventDefault();
@@ -13,10 +13,10 @@ $(function(){
         move[e.target.id] = true;
         e.target.style.background = '#404b69';
     };
-    
+
     $('div').on('touchstart', e => handleTouchStart(e));
     $('div').on('touchend', e => handleTouchEnd(e));
     $('div').on('touchcancel', e => handleTouchEnd(e));
 
-    setInterval(() => socket.emit('moveWeb', move), 1000 / 60);
+    //setInterval(() => socket.emit('moveWeb', move), 1000 / 60);
 });
